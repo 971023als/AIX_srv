a0cfb72a669f07eeb8c58bf5217d02800077163f
971023als <971023als@naver.com>
2024-05-29 10:59:01 +0900

    35 37

--- shell script/SRV-035.sh
+++ shell script/SRV-035.sh
@@ -6,7 +6,7 @@ OUTPUT_CSV="output.csv"
 
 # Set CSV Headers if the file does not exist
 if [ ! -f $OUTPUT_CSV ]; then
-    echo "category,code,riskLevel,diagnosisItem,diagnosisResult,status" > $OUTPUT_CSV
+    echo "category,code,riskLevel,diagnosisItem,service,diagnosisResult,status" > $OUTPUT_CSV
 fi
 
 # Initial Values
@@ -14,13 +14,14 @@ category="시스템 보안"
 code="SRV-035"
 riskLevel="상"
 diagnosisItem="취약한 서비스 활성화 검사"
+service="Service Management"
 diagnosisResult=""
 status=""
 
 BAR
 
 # Write initial values to CSV
-echo "$category,$CODE,$riskLevel,$diagnosisItem,$diagnosisResult,$status" >> $OUTPUT_CSV
+echo "$category,$code,$riskLevel,$diagnosisItem,$service,$diagnosisResult,$status" >> $OUTPUT_CSV
 
 TMP1=$(basename "$0").log
 > $TMP1
@@ -40,13 +41,13 @@ check_service_activation() {
     local service_file=$1
     local service_name=$2
 
-    if [ -f "$service_file" ]; then
+    if [ -f "$service_file" ];then
         local disabled_count=$(grep -vE '^#|^\s#' "$service_file" | grep -i 'disable' | grep -i 'yes' | wc -l)
         if [ $disabled_count -eq 0 ]; then
             diagnosisResult="$service_name 서비스가 ${service_file} 파일에서 실행 중입니다."
             status="취약"
             echo "WARN: $diagnosisResult" >> $TMP1
-            echo "$category,$CODE,$riskLevel,$diagnosisItem,$diagnosisResult,$status" >> $OUTPUT_CSV
+            echo "$category,$code,$riskLevel,$diagnosisItem,$service,$diagnosisResult,$status" >> $OUTPUT_CSV
             cat $TMP1
             echo ; echo
             exit 0
@@ -68,7 +69,7 @@ if [ -f /etc/inetd.conf ]; then
             diagnosisResult="$service 서비스가 /etc/inetd.conf 파일에서 실행 중입니다."
             status="취약"
             echo "WARN: $diagnosisResult" >> $TMP1
-            echo "$category,$CODE,$riskLevel,$diagnosisItem,$diagnosisResult,$status" >> $OUTPUT_CSV
+            echo "$category,$code,$riskLevel,$diagnosisItem,$service,$diagnosisResult,$status" >> $OUTPUT_CSV
             cat $TMP1
             echo ; echo
             exit 0
@@ -79,7 +80,7 @@ fi
 diagnosisResult="취약한 서비스가 비활성화된 경우"
 status="양호"
 echo "OK: $diagnosisResult" >> $TMP1
-echo "$category,$CODE,$riskLevel,$diagnosisItem,$diagnosisResult,$status" >> $OUTPUT_CSV
+echo "$category,$code,$riskLevel,$diagnosisItem,$service,$diagnosisResult,$status" >> $OUTPUT_CSV
 
 BAR
 
