eb74fddc9943739865b4623ad92757d122a8e11f
971023als <971023als@naver.com>
2024-05-29 11:46:20 +0900

    Update SRV-059.sh

--- shell script/SRV-059.sh
+++ shell script/SRV-059.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Load external functions from function.sh
 . function.sh
 
 OUTPUT_CSV="output.csv"
 
 # Set CSV Headers if the file does not exist
 if [ ! -f $OUTPUT_CSV ]; then
-    echo "category,code,riskLevel,diagnosisItem,diagnosisResult,status" > $OUTPUT_CSV
+    echo "category,code,riskLevel,diagnosisItem,service,diagnosisResult,status" > $OUTPUT_CSV
 fi
 
 # Initial Values
@@ -18,17 +17,16 @@ diagnosisItem="웹 서비스 서버 명령 실행 기능 제한 설정 미흡"
 diagnosisResult=""
 status=""
 
-# Define a temporary log file
+BAR
+
+# Write initial values to CSV
+echo "$category,$code,$riskLevel,$diagnosisItem,,$diagnosisResult,$status" >> $OUTPUT_CSV
+
 TMP1=$(basename "$0").log
 > $TMP1
 
-# Print bar (assuming BAR is a defined function)
 BAR
 
-# Print code and description
-echo "[SRV-059] 웹 서비스 서버 명령 실행 기능 제한 설정 미흡" >> $TMP1
-
-# Append evaluation criteria to the log file
 cat << EOF >> $TMP1
 [양호]: 웹 서비스에서 서버 명령 실행 기능이 적절하게 제한된 경우
 [취약]: 웹 서비스에서 서버 명령 실행 기능의 제한이 미흡한 경우
@@ -59,7 +57,7 @@ check_command_execution() {
     echo "INFO: $result_message" >> $TMP1
   fi
   diagnosisResult=$result_message
-  echo "$category,$code,$riskLevel,$diagnosisItem,$diagnosisResult,$status" >> $OUTPUT_CSV
+  echo "$category,$code,$riskLevel,$diagnosisItem,$service,$diagnosisResult,$status" >> $OUTPUT_CSV
 }
 
 # Apache 또는 Nginx 웹 서비스의 서버 명령 실행 제한 설정 확인
