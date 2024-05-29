2b38a5a95f3a2f8134f5f12edf8ab45f24d7e7c7
971023als <971023als@naver.com>
2024-05-29 14:29:02 +0900

    Update SRV-075.sh

--- shell script/SRV-075.sh
+++ shell script/SRV-075.sh
@@ -2,23 +2,31 @@
 
 . function.sh
 
-# Initialize CSV file
-CSV_FILE="$(basename "$0" .sh).csv"
-if [ ! -f $CSV_FILE ]; then
-    echo "Category,Code,Risk Level,Diagnosis Item,DiagnosisResult,Status" > $CSV_FILE
+OUTPUT_CSV="output.csv"
+
+# Set CSV Headers if the file does not exist
+if [ ! -f $OUTPUT_CSV ]; then
+    echo "category,code,riskLevel,diagnosisItem,service,diagnosisResult,status" > $OUTPUT_CSV
 fi
+
+# Initial Values
+category="계정 보안"
+code="SRV-075"
+riskLevel="중"
+diagnosisItem="비밀번호 정책 감사"
+service="Account Management"
+diagnosisResult=""
+status=""
 
 BAR
 
-CATEGORY="계정 보안"
-CODE="SRV-075"
-RISK_LEVEL="중"
-DIAGNOSIS_ITEM="비밀번호 정책 감사"
-DiagnosisResult=""
-Status=""
+# Write initial values to CSV
+echo "$category,$code,$riskLevel,$diagnosisItem,$service,$diagnosisResult,$status" >> $OUTPUT_CSV
 
 TMP1=$(basename "$0").log
 > $TMP1
+
+BAR
 
 cat << EOF >> $TMP1
 [양호]: 암호 정책이 강력하게 설정되어 있는 경우
@@ -31,7 +39,7 @@ BAR
 append_to_csv() {
     local result=$1
     local status=$2
-    echo "$CATEGORY,$CODE,$RISK_LEVEL,$DIAGNOSIS_ITEM,$result,$status" >> $CSV_FILE
+    echo "$category,$code,$riskLevel,$diagnosisItem,$service,$result,$status" >> $OUTPUT_CSV
 }
 
 # Variables initialization
@@ -85,8 +93,8 @@ check_password_policy() {
 }
 
 # Check password policy settings
-check_password_policy "/etc/login.defs" "minlen" "PASS_MIN_LEN" 8 "패스워드 최소 길이가 8 미만으로 설정되어 있습니다."
-for file in "/etc/pam.d/system-auth" "/etc/pam.d/password-auth"; do
+check_password_policy "/etc/security/login.cfg" "minlen" "minalpha" 8 "패스워드 최소 길이가 8 미만으로 설정되어 있습니다."
+for file in "/etc/security/user"; do
     for module in "${input_modules[@]}"; do
         check_password_policy "$file" "minlen" "minlen" 8 "패스워드 최소 길이가 8 미만으로 설정되어 있습니다."
         for option in "${input_options[@]}"; do
@@ -100,41 +108,41 @@ for option in "${input_options[@]}"; do
 done
 
 # Check password maximum usage period
-if [ -f /etc/login.defs ]; then
-    etc_logindefs_maxdays_count=$(grep -vE '^#|^\s#' /etc/login.defs | grep -i 'PASS_MAX_DAYS' | awk '{print $2}' | wc -l)
-    if [ $etc_logindefs_maxdays_count -gt 0 ]; then
-        etc_logindefs_maxdays_value=$(grep -vE '^#|^\s#' /etc/login.defs | grep -i 'PASS_MAX_DAYS' | awk '{print $2}')
-        if [ $etc_logindefs_maxdays_value -gt 90 ]; then
-            append_to_csv "/etc/login.defs 파일에 패스워드 최대 사용 기간이 91일 이상으로 설정되어 있습니다." "취약"
+if [ -f /etc/security/user ]; then
+    etc_security_user_maxdays_count=$(grep -vE '^#|^\s#' /etc/security/user | grep -i 'maxage' | awk '{print $3}' | wc -l)
+    if [ $etc_security_user_maxdays_count -gt 0 ]; then
+        etc_security_user_maxdays_value=$(grep -vE '^#|^\s#' /etc/security/user | grep -i 'maxage' | awk '{print $3}')
+        if [ $etc_security_user_maxdays_value -gt 90 ]; then
+            append_to_csv "/etc/security/user 파일에 패스워드 최대 사용 기간이 91일 이상으로 설정되어 있습니다." "취약"
         else
             append_to_csv "패스워드 최대 사용 기간이 적절하게 설정되어 있습니다." "양호"
         fi
     else
-        append_to_csv "/etc/login.defs 파일에 패스워드 최대 사용 기간이 설정되어 있지 않습니다." "취약"
+        append_to_csv "/etc/security/user 파일에 패스워드 최대 사용 기간이 설정되어 있지 않습니다." "취약"
     fi
 else
-    append_to_csv "/etc/login.defs 파일이 없습니다." "취약"
+    append_to_csv "/etc/security/user 파일이 없습니다." "취약"
 fi
 
 # Check password minimum usage period
-if [ -f /etc/login.defs ]; then
-    etc_logindefs_mindays_count=$(grep -vE '^#|^\s#' /etc/login.defs | grep -i 'PASS_MIN_DAYS' | awk '{print $2}' | wc -l)
-    if [ $etc_logindefs_mindays_count -gt 0 ]; then
-        etc_logindefs_mindays_value=$(grep -vE '^#|^\s#' /etc/login.defs | grep -i 'PASS_MIN_DAYS' | awk '{print $2}')
-        if [ $etc_logindefs_mindays_value -lt 1 ]; then
-            append_to_csv "/etc/login.defs 파일에 패스워드 최소 사용 기간이 1일 미만으로 설정되어 있습니다." "취약"
+if [ -f /etc/security/user ]; then
+    etc_security_user_mindays_count=$(grep -vE '^#|^\s#' /etc/security/user | grep -i 'minage' | awk '{print $3}' | wc -l)
+    if [ $etc_security_user_mindays_count -gt 0 ]; then
+        etc_security_user_mindays_value=$(grep -vE '^#|^\s#' /etc/security/user | grep -i 'minage' | awk '{print $3}')
+        if [ $etc_security_user_mindays_value -lt 1 ]; then
+            append_to_csv "/etc/security/user 파일에 패스워드 최소 사용 기간이 1일 미만으로 설정되어 있습니다." "취약"
         else
             append_to_csv "패스워드 최소 사용 기간이 적절하게 설정되어 있습니다." "양호"
         fi
     else
-        append_to_csv "/etc/login.defs 파일에 패스워드 최소 사용 기간이 설정되어 있지 않습니다." "취약"
+        append_to_csv "/etc/security/user 파일에 패스워드 최소 사용 기간이 설정되어 있지 않습니다." "취약"
     fi
 else
-    append_to_csv "/etc/login.defs 파일이 없습니다." "취약"
+    append_to_csv "/etc/security/user 파일이 없습니다." "취약"
 fi
 
 # Check for shadow password usage
-if [ $(awk -F : '$2!="x"' /etc/passwd | wc -l) -gt 0 ]; then
+if [ $(awk -F : '$2!="!"' /etc/passwd | wc -l) -gt 0 ]; then
     append_to_csv "쉐도우 패스워드를 사용하고 있지 않습니다." "취약"
 else
     append_to_csv "쉐도우 패스워드를 사용하고 있습니다." "양호"
@@ -142,6 +150,6 @@ fi
 
 cat $TMP1
 
-echo "CSV report generated: $CSV_FILE"
+echo "CSV report generated: $OUTPUT_CSV"
 echo ; echo
-cat $CSV_FILE
+cat $OUTPUT_CSV
