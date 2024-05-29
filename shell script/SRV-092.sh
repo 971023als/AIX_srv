2811b2cff087efa05d8bb2e20144e33fd5a407a8
971023als <971023als@naver.com>
2024-05-29 15:29:20 +0900

    Update SRV-092.sh

--- shell script/SRV-092.sh
+++ shell script/SRV-092.sh
@@ -36,9 +36,9 @@ append_to_csv() {
     echo "$CATEGORY,$CODE,$RISK_LEVEL,$DIAGNOSIS_ITEM,$result,$status" >> $CSV_FILE
 }
 
-# /etc/passwd에서 사용자 홈 디렉터리 정보 추출 및 확인
+# /etc/passwd에서 사용자 홈 디렉토리 정보 추출 및 확인
 while IFS=: read -r user _ _ _ _ home_dir _; do
-    if [ ! -d "$home_dir" ] || [ -z "$home_dir" ]; then
+    if [ -z "$home_dir" ] || [ ! -d "$home_dir" ]; then
         DiagnosisResult="사용자 $user 에 대한 홈 디렉터리($home_dir)가 잘못 설정되었습니다."
         Status="취약"
         append_to_csv "$DiagnosisResult" "$Status"
